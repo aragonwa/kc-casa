7ff8a2b88dece154443d934adcb06fb07dee01d0
Aragon <aragonwa@gmail.com>
2017-12-20 12:40:03 -0800

    Addingn routes to main app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Routes from './routes';
+import "bootstrap";
+
+// import './index.css';
+// import App from './App';
 // import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Routes />, document.getElementById('root'));
 // registerServiceWorker();
