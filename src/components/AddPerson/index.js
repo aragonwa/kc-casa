0c736f9194cfce893740d1fa81ca5d1680a48ba4
Aragon <aragonwa@gmail.com>
2017-12-20 12:36:55 -0800

    Adding AddPerson route

---
+++ src/components/AddPerson/index.js
@@ -0,0 +1,20 @@
+// src/components/About/index.js
+import React, { Component } from 'react';
+import './style.css';
+
+export default class AddPerson extends Component {
+  // static propTypes = {}
+  // static defaultProps = {}
+  // state = {}
+
+  render() {
+    // const { ...props } = this.props;
+    return (
+      <div >
+        <h1>
+          Add person
+        </h1>
+      </div>
+    );
+  }
+}
